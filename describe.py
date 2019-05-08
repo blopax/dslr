9f140d240719606cb2bad330e068ae09978bc017
blopax <pablo42clement@gmail.com>
2019-05-08 12:38:47 +0200

    Fix quantile and std

--- describe.py
+++ describe.py
@@ -1,7 +1,5 @@
 import pandas as pd
-# import numpy as np
 
-# from describe_utils import get_count_mean, get_dispertion, get_std
 import describe_utils
 import utils
 
@@ -21,46 +19,7 @@ def describe(df):
     print(df)
 
 
-#
-# def describe(data):
-#     index = ['Count', 'Mean', 'Std', 'Min', '25%', '50%', '75%', 'Max']
-#     for column in data.columns:
-#         if pd.Series(data[column]).dropna().dtype == np.float64:
-#             features.append(column)
-#     describe_df = pd.DataFrame(index=index, columns=features)
-#     print(describe_df)
-#     d = {}
-#     Count = []
-#     Mean = []
-#     Std = []
-#     Min = []
-#     onequart = []
-#     twoquart = []
-#     threequart = []
-#     Max = []
-#     for column in describe_df.columns:
-#         Count.append(get_count_mean(get_clean_series(data, column))[0])
-#         Mean.append(get_count_mean(get_clean_series(data, column))[1])
-#         Std.append(get_std(get_clean_series(data, column), Count[-1], Mean[-1]))
-#         Min.append(get_dispertion(get_clean_series(data, column), Count[-1])[0])
-#         onequart.append(get_dispertion(get_clean_series(data, column), Count[-1])[1])
-#         twoquart.append(get_dispertion(get_clean_series(data, column), Count[-1])[2])
-#         threequart.append(get_dispertion(get_clean_series(data, column), Count[-1])[3])
-#         Max.append(get_dispertion(get_clean_series(data, column), Count[-1])[4])
-#     d['Count'] = Count
-#     d['Mean'] = Mean
-#     d['Std'] = Std
-#     d['Min'] = Min
-#     d['onequart'] = onequart
-#     d['twoquart'] = twoquart
-#     d['threequart'] = threequart
-#     d['Max'] = Max
-#     print(d)
-
-
 if __name__ == "__main__":
     data = pd.read_csv("dataset_train.csv")
-    # count_black_arts = get_count_mean(get_clean_series(data, "Defense Against the Dark Arts"))[0]
     describe(data)
     print(data[utils.SUBJECT].describe())
-    # print(count_black_arts)
--- describe_utils.py
+++ describe_utils.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
 
-def get_centile(serie, count, quantile_nb, quantile):
-    quantile_position = int(quantile_nb / quantile * (count - 1))
-    if ((count - 1) * quantile_nb / quantile) == int((count - 1) * quantile_nb / quantile):
-        quantile_val = float(serie.iloc[quantile_position])
-    else:
-        quantile_val = ((quantile - quantile_nb) * float(serie.iloc[quantile_position])
-                        + quantile_nb * float(serie.iloc[quantile_position + 1])) / quantile
-    return quantile_val
+def get_quantile(serie, count, quantile_nb, quantile):
+    if serie.empty or count <= 0 or quantile == 0:
+        return None
+    quantile_proportion = float(quantile_nb / quantile)
+    float_position = (count - 1) * quantile_proportion
+    floor = int(float_position)
+    if float_position == floor:
+        return serie.iloc[floor]
+    ceil = floor + 1
+    return serie.iloc[floor] * (ceil - float_position) + serie.iloc[ceil] * (float_position - floor)
 
 
 def get_count_mean(serie):
+    if serie.empty:
+        return 0, None
     total, index = 0, 0
     for index, item in enumerate(serie):
         total += item
@@ -21,20 +25,27 @@ def get_count_mean(serie):
 
 
 def get_std(serie, count, mean):
-    deviation_sum = 0
+    if count <= 1:
+        return None
+    deviation_sum = 0.0
+    i = 0
     for item in serie:
-        deviation_sum += ((float(item) - float(mean)) ** 2)
-    std = (float(deviation_sum) / count) ** 0.5
+        deviation_sum += (float(item) - float(mean)) ** 2
+        i += 1
+    std = (float(deviation_sum) / (count - 1)) ** 0.5
     return std
 
 
 def get_dispertion(serie, count):
     sorted_serie = serie.sort_values()
-    minimum = float(sorted_serie.iloc[0])
-    first_quartile = get_centile(sorted_serie, count, 1, 4)
-    median = get_centile(sorted_serie, count, 1, 2)
-    third_quartile = get_centile(sorted_serie, count, 3, 4)
-    maximum = float(sorted_serie.iloc[int(count - 1)])
+    if not serie.empty:
+        minimum = float(sorted_serie.iloc[0])
+        maximum = float(sorted_serie.iloc[int(count - 1)])
+    else:
+        minimum, maximum = None, None
+    first_quartile = get_quantile(sorted_serie, count, 1, 4)
+    median = get_quantile(sorted_serie, count, 1, 2)
+    third_quartile = get_quantile(sorted_serie, count, 3, 4)
     return minimum, first_quartile, median, third_quartile, maximum
 
 
@@ -48,10 +59,7 @@ def describe_serie(serie):
 if __name__ == "__main__":
     df = pd.read_csv("dataset_train.csv")
     S = df["Arithmancy"].dropna()
-    # S = pd.Series([0, 1, 2, 3 , 4 ,5 ,6 ,7])
-
-    S_count, S_mean = get_count_mean(S)
-    std_dev = get_std(S, S_count, S_mean)
-    print(S_count, S_mean, std_dev)
-    print(get_dispertion(S, S_count))
+    # S = pd.Series([])
+    # S = pd.Series([1, 2, 3, 4])
+    print(describe_serie(S))
     print(S.describe())
